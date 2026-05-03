d8062a8838fce2ef15a3b05ddf332b6882f6b2f2
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-05-03 22:49:07 +0530

    chore: init oxfmt

---
+++ oxfmt.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+  semi: true,
+  printWidth: 110,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  jsxSingleQuote: false,
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "^@repo/(.*)$",
+    "<TYPES>",
+    "<TYPES>^[.]",
+    "^@/lib/(.*)$",
+    "^@/components/(.*)$",
+    "^@/app/(.*)$",
+    "^[.]",
+  ],
+  sortTailwindcss: {},
+  sortPackageJson: false,
+  ignorePatterns: [],
+});
